5caddc349bea9eb082ea8759a5d32364144445b0
Guga Gongadze <gugongadze@gmail.com>
2023-01-10 14:39:55 +0400

    fix: how it works section

--- components/sections/HowItWorks.tsx
+++ components/sections/HowItWorks.tsx
@@ -1,11 +1,18 @@
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
 import clsx from 'clsx'
 import { Copy } from 'components/svgs'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 const HowItWorks = () => {
 	const [selectedItem, setSelectedItem] = useState('hello-cloud')
+	const slideRef = useRef<Splide>(null)
+
+	const handleClick = (id: number) => {
+		if (slideRef.current) {
+			slideRef.current.go(id)
+		}
+	}
 
 	return (
 		<div className='mx-auto mt-24 w-full max-w-[1280px] px-5 sm:mt-28 sm:px-10 lg:mt-36 lg:grid lg:grid-cols-[1fr_minmax(566px,_1fr)] lg:gap-7 xl:grid-cols-2 desktop:mt-40'>
@@ -14,92 +21,98 @@ const HowItWorks = () => {
 					How it works
 				</h2>
 				<div className='mt-4 space-y-7 text-[#525151] dark:text-[#7A7A7A] sm:mt-8 lg:text-xl'>
-					<p>Shuttle is built for Rust.</p>
-					<p>
-						A simple cargo command packages up your application, ships it to the shuttle build cluster
-						where it&lsquo;s incrementally compiled and automatically served on a unique subdomain.
-					</p>
-					<p>
-						Shuttle uses simple but powerful annotations to understand your dependencies.
-						Infrastructure dependencies like databases or key-value stores are spun up for you and
-						everything is automatically wired together from the get-go.
-					</p>
-					<p>It feels a little magical.</p>
+					<Splide
+						ref={slideRef}
+						options={{
+							pagination: false,
+							arrows: false,
+						}}
+					>
+						<SplideSlide>
+							<p>
+								With shuttle you can take any Rust service code, and have it ready for deployment
+								by adding a single annotation to your main function.
+							</p>
+						</SplideSlide>
+						<SplideSlide>
+							<p>
+								Your code is packaged and sent to shuttle where it is compiled and deployed. After
+								a few seconds, your app is already running in the cloud.
+							</p>
+						</SplideSlide>
+						<SplideSlide>
+							<p>
+								You can then add more annotations that control cloud resources. Adding components
+								like databases, secrets or storage is as easy as adding arguments to a function.
+							</p>
+						</SplideSlide>
+						<SplideSlide>
+							<p>
+								The next time you deploy your app, shuttle provisions and configures the resources
+								you need. In minutes, you can build complex and scalable apps on industry-standard
+								AWS products and never have to touch the AWS console.
+							</p>
+						</SplideSlide>
+					</Splide>
 				</div>
 			</div>
 			<div className='shadow-gradient mt-14 before:blur-[60px] lg:mt-0'>
-				<Splide
-					options={{
-						pagination: false,
-						arrows: false,
-						autoWidth: true,
-						gap: '0.75rem',
-						mediaQuery: 'min',
-						breakpoints: {
-							768: {
-								gap: 0,
-								drag: false,
-							},
-						},
+				<button
+					className={clsx(
+						'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
+						selectedItem === 'hello-cloud'
+							? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
+							: 'tab-shadow dark:text-[#C2C2C2]'
+					)}
+					onClick={() => {
+						setSelectedItem('hello-cloud')
+						handleClick(0)
 					}}
-					className='splide md:top-px'
-					hasTrack={false}
 				>
-					<SplideTrack className='!overflow-visible'>
-						<SplideSlide>
-							<button
-								className={clsx(
-									'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
-									selectedItem === 'hello-cloud'
-										? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
-										: 'tab-shadow dark:text-[#C2C2C2]'
-								)}
-								onClick={() => setSelectedItem('hello-cloud')}
-							>
-								Hello cloud
-							</button>
-						</SplideSlide>
-						<SplideSlide>
-							<button
-								className={clsx(
-									'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
-									selectedItem === 'cargo-deploy'
-										? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
-										: 'tab-shadow dark:text-[#C2C2C2]'
-								)}
-								onClick={() => setSelectedItem('cargo-deploy')}
-							>
-								Cargo deploy
-							</button>
-						</SplideSlide>
-						<SplideSlide>
-							<button
-								className={clsx(
-									'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
-									selectedItem === 'using-sql'
-										? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
-										: 'tab-shadow dark:text-[#C2C2C2]'
-								)}
-								onClick={() => setSelectedItem('using-sql')}
-							>
-								Using SQL
-							</button>
-						</SplideSlide>
-						<SplideSlide>
-							<button
-								className={clsx(
-									'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
-									selectedItem === 'using-axum'
-										? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
-										: 'tab-shadow dark:text-[#C2C2C2]'
-								)}
-								onClick={() => setSelectedItem('using-axum')}
-							>
-								Using Axum
-							</button>
-						</SplideSlide>
-					</SplideTrack>
-				</Splide>
+					Hello cloud
+				</button>
+				<button
+					className={clsx(
+						'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
+						selectedItem === 'cargo-deploy'
+							? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
+							: 'tab-shadow dark:text-[#C2C2C2]'
+					)}
+					onClick={() => {
+						setSelectedItem('cargo-deploy')
+						handleClick(1)
+					}}
+				>
+					Cargo deploy
+				</button>
+				<button
+					className={clsx(
+						'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
+						selectedItem === 'using-sql'
+							? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
+							: 'tab-shadow dark:text-[#C2C2C2]'
+					)}
+					onClick={() => {
+						setSelectedItem('using-sql')
+						handleClick(2)
+					}}
+				>
+					Using SQL
+				</button>
+				<button
+					className={clsx(
+						'whitespace-nowrap rounded-[1.75rem] p-2 px-6 font-gradual font-bold leading-none md:rounded-b-none md:border-b-0 md:py-3',
+						selectedItem === 'using-axum'
+							? 'border border-[#191919] bg-[#13292C] text-[#D8D8D8] dark:bg-black'
+							: 'tab-shadow dark:text-[#C2C2C2]'
+					)}
+					onClick={() => {
+						setSelectedItem('using-axum')
+						handleClick(3)
+					}}
+				>
+					Using Axum
+				</button>
 				<div className='mt-3 rounded-[2rem] border border-[#191919] bg-[#13292C] p-6 text-right dark:bg-black md:mt-0 md:rounded-tl-none'>
 					<button className='rounded-xl border border-[#191919] p-2 text-white hover:border-[#484848] hover:bg-[#343434] dark:text-[#C2C2C2]'>
 						<Copy width={32} height={32} />
